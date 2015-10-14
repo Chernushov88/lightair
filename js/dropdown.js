760a75bb0e95f533870820d7764781ef5135104e
sergey <chernushov88@mail.ru>
2015-10-14 14:28:23 +0300

    lightair site

---
+++ js/dropdown.js
@@ -0,0 +1,71 @@
+﻿var cookieName = "lightair-lang-cookie";
+var expire = 90;
+
+window.addEvent("domready", function() {
+    var isCookieSet = getCookie(cookieName);
+    var csp = checkIfStartPage();
+    if ((isCookieSet != null && isCookieSet !="") || (!csp)){
+        //window.location.assign(isCookieSet);
+    }
+    else if(typeof geoip_country_code == 'function'){
+        var country= geoip_country_code();
+        var selectedURL = "";
+        
+        if(country == "SE") {
+            selectedURL = "http://www.lightair.se";
+        }
+        else if(country == "CN") {
+          selectedURL = "http://www.lightair.com/cn/";
+        } 
+        else if(country == "FR") {
+          selectedURL = "http://www.lightair.com/fr/";
+        }
+        else if(country == "JP") {
+          selectedURL = "http://www.lightair.com/jp/";
+        }
+        
+        if(selectedURL != ""){
+            setCookieMinutes(cookieName, selectedURL, expire); 
+            window.location.assign(selectedURL);
+        }
+        else{
+            setCookieMinutes(cookieName, "http://www.lightair.com/", expire); 
+        }
+    }
+    
+});
+
+function openDir() {
+    var selected = $('goto').getSelected();
+    setCookieMinutes(cookieName, selected[0].value, expire); 
+    window.location.assign(selected[0].value);
+} 
+
+function getCookie(c_name){
+    var i,x,y,ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++){
+      x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+      y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+      x=x.replace(/^\s+|\s+$/g,"");
+      if (x==c_name){
+        return unescape(y);
+      }
+    }
+}
+
+function setCookieMinutes(name, value, minutes) {
+    var date = new Date();
+    date.setTime(date.getTime()+(minutes*60*1000));
+    var expires = "; expires="+date.toGMTString()+";path=/";
+
+    document.cookie = name+"="+value+expires;
+}
+
+function checkIfStartPage(){
+    var url = document.URL;
+    var urls = ["http://www.lightair.com/", "http://www.lightair.se/", "http://www.lightair.com/cn/",
+        "http://www.lightair.com/fr/", "http://www.lightair.com/jp/", "http://www.lightair.com/show.php", 
+        "http://lightair.com/", "http://lightair.se/", "http://lightair.com/cn/", "http://lightair.com/fr/", 
+        "http://lightair.com/jp/", "http://lightair.com/show.php"];
+    return (urls.indexOf(url) > -1);
+}
---
+++ js/tabs.js
@@ -0,0 +1,136 @@
+﻿// Tabs
+tab_count = 0;
+
+
+
+function make_tab_group_of_tab(tab) {
+  
+  if (tab.tagName.toLowerCase() != 'h5') {
+    return;
+  }
+  var tab_group_start = tab;
+  var element = tab;
+  var tab_group_navigation = new Element('ul');
+  var current_tab = tab;
+  var current_tab_content = null;
+  var current_tab_container = null;
+  var tab_container;
+  var li = null;
+  var a = null;
+  var is_first_tab = true;
+  
+  while (!element.getParent().hasClass('mainslot')) {
+    element = element.getParent();
+  }
+  
+  if (element.belongs_to_tab_group != null) {
+    return;
+  }
+  
+  
+  tab_group_navigation.addClass('tab-navigation');
+  
+  while (true) {
+    element.belongs_to_tab_group = tab_group_start;
+    
+    if (!is_tab_end(element)) {
+    if(current_tab_content != null)
+            current_tab_content.push(element);
+    }
+    
+    if (is_tab_end(element) || indicates_tab_group_end(element.getNext())) {
+      if (current_tab_content != null) {
+        current_tab_content.each(function(tab_content) {
+          tab_content.injectInside(current_tab_container);
+        });
+        if (!is_first_tab) {
+          current_tab_container.style.display = 'none';
+        }
+        current_tab_container.injectAfter(current_tab);
+        is_first_tab = false;
+        // console.log(current_tab_container.innerHTML);
+      }
+      current_tab = element;
+      // current_tab.style.display = 'none';
+      current_tab_content = new Array();
+      current_tab_container = new Element('div');
+      current_tab_container.addClass('tab-container');
+      current_tab_container.id = 'tab-' + (++tab_count);
+      // console.log(current_tab_container.id);
+      
+      if (current_tab.getElements('h5.tab').length > 0) {
+        li = new Element('li');
+        a = new Element('a');
+        a.href = '#tab-' + tab_count;
+        a.set('text', current_tab.get('text').replace(/\r?\n/g,""));
+        a.injectInside(li);
+        if (is_first_tab) {
+          li.addClass('active');
+        }
+        li.injectInside(tab_group_navigation);
+      }
+    }
+    
+    element = element.getNext();
+    if (indicates_tab_group_end(element)) {
+      break;
+    }
+  }
+  
+  tab_group_navigation.injectBefore(tab_group_start);
+}
+
+function indicates_tab_group_end(element) {
+  if (typeof(element) == 'undefined' || element == null) {
+    // console.log('group end? undef or null!');
+    return true;
+  } else if (element.getElements('.tab-end').length > 0) {
+    // console.log('group end? tab end');
+    return true;
+  }
+  // console.log('group end? no');
+  return false;
+}
+
+function is_tab_end(element) {
+  if (element.getElements('h5.tab').length > 0) {
+    return true;
+  } else if (element.getElements('.tab-end').length > 0) {
+    return true;
+  }
+  return false;
+}
+
+function tab_click() {
+  // console.log(this);
+  var li = this.getParent();
+  var this_tab_content = $(this.hash.replace('#', ''));
+  
+  li.getParent().getElements('a').each(function(element) {
+    var tab_content = $(element.hash.replace('#', ''));
+    if (tab_content) {
+      tab_content.style.display = 'none';
+    }
+    element.getParent().removeClass('active');
+  });
+  
+  if (this_tab_content) {
+    this_tab_content.style.display = 'block';
+  }
+  this.getParent().addClass('active');
+  return false;
+}
+
+function apply_tabs() {
+  $$('.tab').each(make_tab_group_of_tab);
+  $$('h5.tab').setStyle('display', 'none');
+  $$('ul.tab-navigation a').each(function(element) {
+    element.onclick = tab_click;
+  });
+    try {
+        window["fixTabParentsClass"]();
+    }
+    catch(err) {}
+}
+
+window.addEvent('domready', apply_tabs);
